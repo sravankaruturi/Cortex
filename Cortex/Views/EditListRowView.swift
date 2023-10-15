6930ff2af3f41d110e5d8b3a1efedde8930c1bfd
Sravan <sravan.karuturi@strivr.com>
2023-10-15 10:47:10 -0700

    Editing items possible. Change of Schema. Using Swiftdata instead of Core Data.

---
+++ Cortex/Views/EditListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  EditListRowView.swift
+//  Cortex
+//
+//  Created by Sravan Karuturi on 10/15/23.
+//
+
+import SwiftUI
+
+struct EditListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditListRowView()
+}
