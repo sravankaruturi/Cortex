3a7f49563f95a23f23f336d6b580f515c7a379ec
Sravan <sravan.karuturi@strivr.com>
2024-06-22 20:50:14 -0700

    Cleaned unused files. Added unit tests for the Color Extensions.

--- Cortex/Utils/Ext+Color.swift
+++ Cortex/Utils/Ext+Color.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftUI
 
-extension Color : RawRepresentable, Codable {
+extension Color : @retroactive RawRepresentable, Codable {
     
     public init?(rawValue: String) {
         
---
+++ CortexTests/CortexTests.swift
@@ -0,0 +1,18 @@
+//
+//  CortexTests.swift
+//  CortexTests
+//
+//  Created by Sravan Karuturi on 6/22/24.
+//
+
+import Testing
+
+struct CortexTests {
+
+    @Test func sampleTest() async throws {
+        
+        #expect(1 == 1)
+        
+    }
+
+}
